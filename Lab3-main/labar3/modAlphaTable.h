7fd7ffcec1a8b5c1c078dd5e5054197648098060
TRelm <yagolnitskiy128@gmail.com>
2024-12-25 14:29:52 +0300

    l

--- Lab3-main/labar3/modAlphaTable.h
+++ Lab3-main/labar3/modAlphaTable.h
@@ -4,7 +4,7 @@
 #include <map>
 #include <codecvt>
 #include <locale>
-class modAlphaCipher
+class modAlphaTable
 {
 private:
 	std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
@@ -17,8 +17,8 @@ private:
 	std::wstring getValidOpenText(const std::wstring & ws);
 	std::wstring getValidCipherText(const std::wstring & ws);
 public:
-	modAlphaCipher()=delete;//запретим конструктор без параметров
-	modAlphaCipher(const std::wstring& wskey);  //конструктор для установки ключа
+	modAlphaTable()=delete;//запретим конструктор без параметров
+	modAlphaTable(const std::wstring& wskey);  //конструктор для установки ключа
 	std::wstring encrypt(const std::wstring& open_text);  //зашифрование
 	std::wstring decrypt(const std::wstring& cipher_text);//расшифрование
 };
